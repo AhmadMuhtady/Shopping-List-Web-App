fa1845637ddc2269e0df3e539b2d7278e7164543
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-06-28 04:34:55 +0300

    Enhance item management: implement item removal, clear all functionality, and add marked styling

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 //functions:
 const addItem = (e) => {
@@ -15,17 +17,20 @@ const addItem = (e) => {
 
 	// create list Item
 	const li = document.createElement('li');
-	const itemText = document.createTextNode(newItem);
+	const itemSpan = document.createElement('span');
+	itemSpan.textContent = newItem;
 
 	const buttonDelete = createButtonDelete('remove-item btn-link text-red');
 	const buttonMark = createButtonMark('mark-item btn-link text-green');
 
 	li.appendChild(buttonMark);
-	li.appendChild(itemText);
+	li.appendChild(itemSpan);
 	li.appendChild(buttonDelete);
 
 	itemList.appendChild(li);
+	checkUI();
 	itemInput.value = '';
+	itemInput.focus();
 };
 // create a button that will be inside the Li and has the classes we want
 const createButtonDelete = (classes) => {
@@ -49,5 +54,48 @@ const createIcon = (classes) => {
 	icon.className = classes;
 	return icon;
 };
+
+// removing the li
+
+const removeItem = (e) => {
+	if (e.target.parentElement.classList.contains('remove-item')) {
+		if (confirm('Do you want to delete item?')) {
+			e.target.parentElement.parentElement.remove();
+
+			checkUI();
+		}
+	}
+};
+
+const clearItems = () => {
+	if (confirm('Do you want to delete All item?')) {
+		while (itemList.firstChild) {
+			itemList.removeChild(itemList.firstChild);
+		}
+	}
+	checkUI();
+};
+const markItem = (e) => {
+	if (e.target.parentElement.classList.contains('mark-item')) {
+		const li = e.target.parentElement.parentElement;
+		const span = li.querySelector('span');
+		span.classList.toggle('marked');
+	}
+};
+
+const checkUI = () => {
+	const items = itemList.querySelectorAll('li');
+	if (items.length === 0) {
+		clearBtn.style.display = 'none';
+		itemFilter.style.display = 'none';
+	} else {
+		clearBtn.style.display = 'block';
+		itemFilter.style.display = 'block';
+	}
+};
 //Event listeners:
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemList.addEventListener('click', markItem);
+clearBtn.addEventListener('click', checkUI);
